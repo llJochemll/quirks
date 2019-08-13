4cb90a1a51a3367d1041bb3bfd604026716c34c3
Jochem <jochemdejaeghere@hotmail.com>
2019-08-13 19:02:49 +0200

    add missing imports in package module

--- source/quirks/package.d
+++ source/quirks/package.d
@@ -1,6 +1,8 @@
 module quirks;
 
 public import quirks.aggregate;
+public import quirks.core;
+public import quirks.expression;
 public import quirks.functional;
 public import quirks.tuple;
 public import quirks.type;
