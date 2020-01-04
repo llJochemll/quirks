069c7b965d4c0cadc52337e244bbed4ee23323db
Jochem De Jaeghere <jochem@cloudclass.be>
2020-01-04 16:51:59 +0100

    Fix build error

--- source/quirks/internal/test.d
+++ source/quirks/internal/test.d
@@ -1,51 +1,53 @@
 module quirks.internal.test;
 
-public import fluent.asserts;
+version (unittest) {
+    public import fluent.asserts;
 
-struct TestStruct {
-    static long classifier;
+    struct TestStruct {
+        static long classifier;
 
-    long id;
-    int age;
+        long id;
+        int age;
 
-    static TestStruct create() {
-        return TestStruct();
+        static TestStruct create() {
+            return TestStruct();
+        }
+
+        string name() {
+            return "name";
+        }
+        void update() { }
+        void update(bool force) { }
+    } unittest {
+        TestStruct s;
+
+        s.create.should.not.throwAnyException;
+        s.name.should.equal("name");
+        s.update.should.not.throwAnyException;
+        s.update(false).should.not.throwAnyException;
     }
 
-    string name() {
-        return "name";
+    class TestClass {
+        static long classifier;
+
+        long id;
+        int age;
+
+        static TestClass create() {
+            return new TestClass();
+        }
+
+        string name() {
+            return "name";
+        }
+        void update() { }
+        void update(bool force) { }
+    } unittest {
+        auto c = new TestClass;
+
+        c.create.should.not.throwAnyException;
+        c.name.should.equal("name");
+        c.update.should.not.throwAnyException;
+        c.update(false).should.not.throwAnyException;
     }
-    void update() { }
-    void update(bool force) { }
-} unittest {
-    TestStruct s;
-
-    s.create.should.not.throwAnyException;
-    s.name.should.equal("name");
-    s.update.should.not.throwAnyException;
-    s.update(false).should.not.throwAnyException;
-}
-
-class TestClass {
-    static long classifier;
-
-    long id;
-    int age;
-
-    static TestClass create() {
-        return new TestClass();
-    }
-
-    string name() {
-        return "name";
-    }
-    void update() { }
-    void update(bool force) { }
-} unittest {
-    auto c = new TestClass;
-
-    c.create.should.not.throwAnyException;
-    c.name.should.equal("name");
-    c.update.should.not.throwAnyException;
-    c.update(false).should.not.throwAnyException;
 }
